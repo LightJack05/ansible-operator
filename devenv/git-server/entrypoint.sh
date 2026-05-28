447e403be0cd51ad6701b742f00956755b0de6ea
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-05-28 10:50:55 +0200

    Dynamically build the repos on git server startup to avoid permissions

--- devenv/git-server/entrypoint.sh
+++ devenv/git-server/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Create test repos to be able to serve them via git
+for DIR in $(ls /data); do
+    git init --bare /srv/git/$DIR -b main
+    git --git-dir=/srv/git/$DIR --work-tree=/data/$DIR add -A
+    git --git-dir=/srv/git/$DIR --work-tree=/data/$DIR commit -m "Initial Commit"
+done
+
 # Install the authorized key from the mount (if present).
 if [ -f /tmp/authorized_keys.pub ]; then
     install -o git -g git -m 600 /tmp/authorized_keys.pub /home/git/.ssh/authorized_keys
